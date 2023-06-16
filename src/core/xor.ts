7a5e25bd07853f5f053e73b78d9090e4bb2064a5
Miriam Zusin <miriamzusin@gmail.com>
2023-06-16 09:35:52 +0300

    xor

--- src/core/xor.ts
+++ src/core/xor.ts
@@ -4,7 +4,7 @@
  * Self-inverse property: x XOR 0 = x
  * Self-inverse property: x XOR x = 0
  * Commutativity: x XOR y = y XOR x
- * XOR operation is not associative: `(a XOR b) XOR c` is NOT equal to `a XOR (b XOR c)`.
+ * Associative property: `(a XOR b) XOR c` equals to `a XOR (b XOR c)`.
  * Equation like `a = b ^ x` or `a = x ^ b` can be solved like: `x = a ^ b`
  * Distributive property of XOR over addition: `A XOR (B + C)` = (A XOR B) + (A XOR C)`
  */
